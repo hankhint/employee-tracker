31ed83ec8a171e37a5b685d229ea730bd1539b52
HankHint <hankhint@gmail.com>
2022-03-04 13:10:29 -0600

    added connect function

--- server.js
+++ server.js
@@ -2,3 +2,30 @@
 import mysql2 from 'mysql2';
 import inquirer from 'inquirer';
 import contable from 'console.table';
+
+//dotenv keeps my sql credentials off github and therefore keeps them confidential
+import dotenv from 'dotenv';
+dotenv.config();
+
+//node -r dotenv
+
+//function to start mysql2 server
+const connection = createConnection({
+    host: 'localhost',
+    //mysql username
+    user: 'root',
+
+//port
+    port: '3306',
+    //env holding my password
+    password: process.env.SECRET_KEY,
+    //database name
+    database: 'employeeDatabase'
+});
+
+connection.connect((err) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+})
