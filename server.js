fbcb6a705a3761afa681c7d5e9564f934e7b833d
HankHint <hankhint@gmail.com>
2022-03-04 13:25:06 -0600

    added inquirer prompts

--- server.js
+++ server.js
@@ -23,10 +23,47 @@ const connection = createConnection({
     database: 'employeeDatabase'
 });
 
+//function to connect to database
 connection.connect((err) => {
     if(err) {
+        //returns error if it fails to connect
         console.log(err);
+        //breaks out of this function and does not get to the message below
         return;
     }
     console.log(`Welcome to the employee database. ID: ${connection.threadId}`)
 })
+//this function is called at the end of the file
+//inquirer prompts stored here
+//menu selections direct to control statements for the selected feature
+const startTrackerPrompts = () => {
+    inquirer.prompt ([
+    {
+        type: 'list',
+        name: 'menuChoices',
+        message: 'What would you like to do?',
+        choices: [
+            'View all employees',
+            'View employees by department',
+            'View employees by manager',
+            'View all departments',
+            'View department budgets',
+            'View all roles',
+            'Add employee',
+            'Add department',
+            'Add role',
+            'Delete employee',
+            'Delete role',
+            'Update employee role',
+            'Update manager of employee',
+            'Exit'
+        ]
+    }
+    ])
+    .then((answers) => {
+        //destructure inputs from user
+        const { menuChoices } = answers;
+
+        //control statements for each menu option
+    })
+};
