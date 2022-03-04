b6051ba8c2dd5d9c9fc2b413c0d6c9fc25fda5c3
HankHint <hankhint@gmail.com>
2022-03-04 14:43:52 -0600

    added schema for employees

--- db/schema.sql
+++ db/schema.sql
@@ -19,4 +19,15 @@ CREATE TABLE roles (
     salary DECIMAL(8,2) NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
-);
+)
+
+--create table for employees
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(60) NOT NULL,
+    last_name VARCHAR(60) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+)
